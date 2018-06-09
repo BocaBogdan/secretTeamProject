87a60c145f1d919e971f00487a4d6398a9f0df83
victor.boca <victor.boca@cgm.com>
2018-06-09 23:02:36 +0200

    Make logout to work

--- logout.php
+++ logout.php
@@ -1,8 +1,8 @@
 <?php
-require_once 'core/init.php';
+    require_once 'core/init.php';
 
-$user = new User();
-$user->logout();
+    $user = new User();
+    $user->logout();
 
-Redirect::redirectTo('index.php');
+    header('Location: index.php ');
 ?>
